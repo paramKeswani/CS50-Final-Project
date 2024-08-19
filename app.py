fbb520004b11b0545517121b1eea7d0ba1c0834e
Tyler <mtjackson0011@gmail.com>
2024-08-18 21:33:02 -0500

    init commit

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("hello")
+from flask import Flask, flash, redirect, render_template, request, session
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("/index.html")
